03ac1f19f5d998860ccd934ed3287bf483d3a248
Bartosz Girzecki <53236740+B4GG1@users.noreply.github.com>
2026-05-25 15:17:56 +0200

    test(AQA-0015): add initial playwright smoke test

---
+++ tests/test_smoke_login.py
@@ -0,0 +1,9 @@
+from playwright.sync_api import Page, expect
+
+def test_sauce_demo_smoke(page : Page):
+    # Arrange
+    url = "https://www.saucedemo.com/"
+    # Act
+    page.goto(url)
+    # Assert
+    expect(page).to_have_title("Swag Labs")
