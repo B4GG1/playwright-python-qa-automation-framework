222c46e077e3ed99f5be3af4fab87b967e878f5d
Bartosz Girzecki <53236740+B4GG1@users.noreply.github.com>
2026-05-30 19:38:48 +0200

    test(AQA-0030): implement positive login scenario

---
+++ tests/test_login_positive.py
@@ -0,0 +1,14 @@
+import re
+
+from playwright.sync_api import Page, expect
+
+from pages.login_page import LoginPage
+from test_data.login_test_data import VALID_USER
+
+
+def test_valid_user_can_log_in_successfully(page: Page):
+    login_page = LoginPage(page)
+    login_page.open()
+    login_page.login(VALID_USER["username"], VALID_USER["password"])
+    expect(page).to_have_url(re.compile(".*inventory.html"))
+    expect(page.locator("[data-test='inventory-container']")).to_be_visible()
