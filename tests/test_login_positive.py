6991e5d75a94c9394e5e9879038c9815229a54d5
Bartosz Girzecki <53236740+B4GG1@users.noreply.github.com>
2026-06-03 18:46:34 +0200

    test(AQA-0037): add login submit with enter key test

--- tests/test_login_positive.py
+++ tests/test_login_positive.py
@@ -15,3 +15,20 @@ def test_valid_user_can_log_in_successfully(opened_login_page: LoginPage, case):
     opened_login_page.login(case["username"], case["password"])
     expect(opened_login_page.page).to_have_url(INVENTORY_URL_PATTERN)
     expect(opened_login_page.page.locator("[data-test='inventory-container']")).to_be_visible()
+
+
+@pytest.mark.regression
+@pytest.mark.ui
+@pytest.mark.positive
+@pytest.mark.parametrize(
+    "_case_id",
+    ["TC-LOGIN-012"],
+    ids=["TC-LOGIN-012"],
+)
+def test_user_can_log_in_by_pressing_enter_key(opened_login_page: LoginPage, _case_id: str):
+    case = VALID_USER_CASES[0]
+    opened_login_page.get_username_input().fill(case["username"])
+    opened_login_page.get_password_input().fill(case["password"])
+    opened_login_page.get_password_input().press("Enter")
+    expect(opened_login_page.page).to_have_url(INVENTORY_URL_PATTERN)
+    expect(opened_login_page.page.locator("[data-test='inventory-container']")).to_be_visible()
