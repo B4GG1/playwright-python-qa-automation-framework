e3f3c68ccde3f21ea45afc0a08a7e1429a4e4e39
Bartosz Girzecki <53236740+B4GG1@users.noreply.github.com>
2026-06-17 17:34:31 +0200

    test(AQA-0057): add product to cart test

--- tests/test_cart_page.py
+++ tests/test_cart_page.py
@@ -3,6 +3,9 @@ from playwright.sync_api import expect
 
 from pages.cart_page import CartPage
 from pages.inventory_page import InventoryPage
+from test_data.inventory_test_data import LIST_OF_PRODUCTS
+
+EXAMPLE_PRODUCT = LIST_OF_PRODUCTS[0]
 
 
 @pytest.mark.smoke
@@ -13,3 +16,13 @@ def test_open_cart_page_from_inventory(logged_in_inventory_page: InventoryPage, 
     cart_page = logged_in_inventory_page.open_cart()
     expect(cart_page.page).to_have_url(CartPage.URL)
     expect(cart_page.get_cart_contents_container()).to_be_visible()
+
+
+@pytest.mark.regression
+@pytest.mark.positive
+@pytest.mark.ui
+@pytest.mark.parametrize("_case_id", ["TC-CART-003+TC-CART-007"], ids=["TC-CART-003+TC-CART-007"])
+def test_user_can_add_product_to_cart(logged_in_inventory_page: InventoryPage, _case_id: str):
+    logged_in_inventory_page.add_product_to_cart(EXAMPLE_PRODUCT["product_name"])
+    cart_page = logged_in_inventory_page.open_cart()
+    expect(cart_page.get_product_card_by_name(EXAMPLE_PRODUCT["product_name"])).to_be_visible()
